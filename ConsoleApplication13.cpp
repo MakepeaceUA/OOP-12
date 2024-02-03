3113869c98f0a47abe1d007872e0ae24f6eff136
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2024-02-03 16:25:46 +0200

    Add files via upload

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+class Swapper
+{
+public:
+   static void Swap(T& f_num,T& s_num) 
+   {
+       T temp = f_num;
+       f_num = s_num;
+       s_num = temp;
+   }
+};
+
+
+
+int main()
+{
+    int x, y;
+    cin >> x;
+    cin >> y;
+    cout << "\n\n";
+
+    Swapper<int>::Swap(x,y); 
+
+    cout  << x << "\n";
+    cout <<  y << "\n";
+
+}
+
+
